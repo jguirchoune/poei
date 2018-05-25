fc1890c8e343a0444ea5150d347d238394b585b9
user9 <user9@gmail.com>
2018-05-25 17:00:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# user9
+# lemonde
---
+++ themes/ive/js/ive.js
@@ -0,0 +1,11 @@
+(function ($, Drupal, drupalSettings) {
+    $(document).ready(function() {
+        /*alert('uid : ' + drupalSettings.user.uid);
+        alert('langue : ' + drupalSettings.path.currentLanguage);
+        console.log('Drupal : ');
+        console.log(Drupal);
+        console.log('drupalSettings : ');
+        console.log(drupalSettings);
+        alert('Hello !');*/
+    });
+})(jQuery, Drupal, drupalSettings);
